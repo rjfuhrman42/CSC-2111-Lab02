a5d61c619fc39d301c0de72cfb5aa561e9493f1f
Michael Hall <mohall42@students.tntech.edu>
2017-01-28 22:54:39 -0600

    I think i did  most of it correctly but i am having trouble on the last function

---
+++ ReadFile_class.cpp
@@ -0,0 +1,44 @@
+#include "ReadFile_class.h"
+#include <iostream>
+#include <string>
+
+ReadFile::ReadFile(const char* file_name) //Contructor. I am unsure if this is correct. 
+{
+  
+   input_file.open(file_name);
+   closed = false;
+   eof = false;
+
+   
+}
+
+ReadFile::~ReadFile() //Destructor. Im not passing anything so what am i destroying???
+{
+   
+}
+
+bool ReadFile::_eof()
+{
+   return eof;
+}
+
+void ReadFile::close()
+{
+   if (closed = false)
+   {
+      input_file.close();
+      closed = true;
+   }
+}
+
+String* ReadFile::readline()
+{
+   if (closed = true) return NULL;
+   if (eof = true) return NULL;
+
+   string text;
+	_eof = !(getline(rf->input_file, text));
+
+   String* str = new String((const char*) text.c_str());
+   return str;
+}
---
+++ ReadFile_class.h
@@ -0,0 +1,32 @@
+#if !defined READ_FILE
+#define READ_FILE
+
+#include "Text.h"
+
+#include <fstream>
+using namespace std;
+
+class ReadFile
+{
+	private:
+		
+		ifstream input_file;
+		bool eof;
+		bool closed;
+	public:
+	
+		ReadFile(const char* file_name);
+		~ReadFile();
+		String* readline();
+		bool _eof();
+		void close();
+	
+};		
+
+/* ReadFile* createReadFile(const char* file_name);
+void destroyReadFile(ReadFile* rf);
+String* readLine(ReadFile* rf);
+bool eof(ReadFile* rf);
+void close(ReadFile* rf); */
+
+#endif
